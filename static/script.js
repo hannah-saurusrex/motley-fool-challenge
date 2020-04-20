f5c45c4e194c55e2289abb7b9d6574212c82ae7a
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-04-20 13:16:52 -0600

    removed search function because it was out of scope; styled market table

--- static/script.js
+++ static/script.js
@@ -1,13 +1,15 @@
 // ToDos: 
 // API call ✅
-// search 
-// filter
+// search ✅
+// filter via topic dropdown ✅
+// filter via topic tags
+// .slice(0,5) results to bring in top 5
+// format published_at timestamp
 // modal??
 const articlesEl = document.getElementById('articles');
-const searchEl = document.getElementById('search');
+// const searchEl = document.getElementById('search');
 const filterBtn = document.getElementById('filter');
 const topicFilter = filterBtn.querySelectorAll('li');
-
 
     
 function displayArticles(articles) {
@@ -22,7 +24,7 @@ function displayArticles(articles) {
                 <img
                     src="${article.images[0].image}"
                     alt="${article.images[0].name}"
-                />
+                    />
                 <h4 class="article-byline">${article.byline} | ${article.publish_at}</h4>
             </div>
             <div class="article-body">
@@ -52,21 +54,21 @@ filterBtn.addEventListener('click', () => {
 });
 
 // search input function
-searchEl.addEventListener('input', e => {
-    const { value } = e.target;
-    const articleHeadline = document.querySelectorAll('.article-headline');
+// searchEl.addEventListener('input', e => {
+//     const { value } = e.target;
+//     const articleHeadline = document.querySelectorAll('.article-headline');
 
-    articleHeadline.forEach(headline => {
-        if (headline.innerText.toLowerCase().includes(value.toLowerCase())) {
-            headline.parentElement.parentElement.style.display = 'block';
-        } else {
-            headline.parentElement.parentElement.style.display = 'none';
-        }
-    });
-});
+//     articleHeadline.forEach(headline => {
+//         if (headline.innerText.toLowerCase().includes(value.toLowerCase())) {
+//             headline.parentElement.parentElement.style.display = 'block';
+//         } else {
+//             headline.parentElement.parentElement.style.display = 'none';
+//         }
+//     });
+// });
 
 
-// filter articles by selected topic
+// filter articles via topic dropdown
 topicFilter.forEach(filter => {
     filter.addEventListener('click', () => {
         const value = filter.innerText;
@@ -81,4 +83,6 @@ topicFilter.forEach(filter => {
         });
     });
 });
+
+// filter articles via topic tag
 
